94953b4d5e767dcada04537abab86ea514a10916
202248 <shuntod2@gmail.com>
2024-01-30 10:34:38 +0800

    s

--- GISFirstDjango/models.py
+++ GISFirstDjango/models.py
@@ -4,6 +4,21 @@ from django.db import models
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    age = models.IntegerField(null=True)
+    gender = models.CharField(max_length=1)
+
+
+class Student(models.Model):
+    name = models.OneToOneField(Person, null=True, on_delete=models.SET_NULL)
+    grade = models.CharField(max_length=2)
+    gmail = models.EmailField()
+
+
+class Teacher(models.Model):
+    name = models.OneToOneField(Person, null=True, on_delete=models.SET_NULL)
+    subject = models.CharField(max_length=30)
+
 
 class TeachingGroup(models.Model):
-    students = models.OneToOneField(Person, null=True, on_delete=models.SET_NULL)
+    teacher = models.OneToOneField(Teacher, null=True, on_delete=models.SET_NULL)
+    students = models.ManyToManyField(Student, null=True)
